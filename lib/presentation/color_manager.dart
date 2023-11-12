2732a6696302d8ac4c2c2e4544c253eba98b1c5f
esraa-mohamad <78769599+esraa-mohamad@users.noreply.github.com>
2023-11-12 23:30:43 +0200

    presentation_color_manager

---
+++ lib/presentation/color_manager.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class ColorManager
+{
+  static Color primaryColor = const Color(0xffED9728);
+  static Color darkGreyColor = const Color(0xff525252);
+  static Color greyColor = const Color(0xff737477);
+  static Color lightGreyColor = const Color(0xff9E9E9E);
+}
