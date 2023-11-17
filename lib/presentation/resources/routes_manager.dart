e661881266c7486aab42681ac600bc2ae9274e75
esraa-mohamad <78769599+esraa-mohamad@users.noreply.github.com>
2023-11-17 19:48:38 +0200

    presentation_finishing_routes_manager

--- lib/presentation/resources/routes_manager.dart
+++ lib/presentation/resources/routes_manager.dart
@@ -1,9 +1,53 @@
+import 'package:flutter/material.dart';
+
+import '../forget_password/forget_password_view.dart';
+import '../login/login_view.dart';
+import '../main/main_view.dart';
+import '../register/register_view.dart';
+import '../splash/splash_view.dart';
+import '../store_details/store_details_view.dart';
+
 class Routes
 {
   static const splashRoute = "/";
   static const loginRoute = "/login";
   static const registerRoute = "/register";
-  static const forgetPasswordRoute = "/forgetPassword";
+  static const forgotPasswordRoute = "/forgetPassword";
   static const mainRoute = "/main";
   static const storeDetailsRoute = "/storeDetails";
 }
+
+class RouteGenerator {
+  static Route<dynamic> getRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case Routes.splashRoute:
+        return MaterialPageRoute(builder: (_) => const SplashView());
+      case Routes.loginRoute:
+        return MaterialPageRoute(builder: (_) => const LoginView());
+      case Routes.registerRoute:
+        return MaterialPageRoute(builder: (_) => const RegisterView());
+      case Routes.forgotPasswordRoute:
+        return MaterialPageRoute(builder: (_) => const ForgetPasswordView());
+      case Routes.mainRoute:
+        return MaterialPageRoute(builder: (_) => const MainView());
+      case Routes.storeDetailsRoute:
+        return MaterialPageRoute(builder: (_) => const StoreDetailsView());
+      default:
+        return unDefinedRoute();
+    }
+  }
+  static Route<dynamic> unDefinedRoute() {
+    return MaterialPageRoute(
+        builder: (_) => Scaffold(
+          appBar: AppBar(
+            title: const Text(
+                "No Route Found"), // todo move this string to strings manager
+          ),
+          body: const Center(
+              child: Text(
+                  "No Route Found")), // todo move this string to strings manager
+        ));
+  }
+}
+
+
