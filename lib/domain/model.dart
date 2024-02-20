32f1a20af17344c42f2bb6eae3400c294d3de4c2
esraa-mohamad <78769599+esraa-mohamad@users.noreply.github.com>
2024-02-20 23:00:02 +0200

    model and extension

--- lib/domain/model.dart
+++ lib/domain/model.dart
@@ -21,3 +21,27 @@ class SliderViewObject
   SliderViewObject(this.sliderObject , this.numOfSlides , this.currentIndex);
 
 }
+// login models
+
+class Customer{
+  String  id ;
+  String  name ;
+  int  numberOfNotifications ;
+
+  Customer({required this.id , required this.name , required this.numberOfNotifications});
+}
+
+class Contact{
+  String  phone ;
+  String  email ;
+  String  link ;
+
+  Contact({required this.phone , required this.email , required this.link});
+}
+
+class Authentication{
+  Customer  customer ;
+  Contact  contact ;
+
+  Authentication({required this.customer , required this.contact});
+}
