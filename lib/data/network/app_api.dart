40f76659b60d2353e005cfa10ae2bf025ccd7d21
esraa-mohamad <78769599+esraa-mohamad@users.noreply.github.com>
2024-02-20 22:43:11 +0200

    app api json

---
+++ lib/data/network/app_api.dart
@@ -0,0 +1,18 @@
+import 'package:advanced_flutter/app/constants.dart';
+import 'package:dio/dio.dart';
+import 'package:retrofit/http.dart';
+
+import '../response/responses.dart';
+part 'app_api.g.dart';
+
+@RestApi(baseUrl: Constants.baseUrl)
+abstract class AppServicesClient {
+
+  factory AppServicesClient(Dio dio , {String baseUrl}) = _AppServicesClient;
+
+  @POST("/customers/login")
+  Future<AuthenticationResponse> login(
+      @Field("email") String email ,
+      @Field("password") String password
+      );
+}
