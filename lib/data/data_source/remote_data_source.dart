c407cf91533ab1f3e3ccd021478ce05a31f42b69
esraa-mohamad <78769599+esraa-mohamad@users.noreply.github.com>
2024-02-21 19:25:08 +0200

    remote data source

---
+++ lib/data/data_source/remote_data_source.dart
@@ -0,0 +1,24 @@
+import 'package:advanced_flutter/data/network/app_api.dart';
+import 'package:advanced_flutter/data/network/requests.dart';
+import 'package:advanced_flutter/data/response/responses.dart';
+
+abstract class RemoteDataSource{
+
+  Future<AuthenticationResponse> login(LoginRequests loginRequests);
+}
+
+class RemoteDataSourceImplementation implements RemoteDataSource {
+
+  final AppServicesClient _appServicesClient;
+
+  RemoteDataSourceImplementation(this._appServicesClient);
+
+  @override
+  Future<AuthenticationResponse> login(LoginRequests loginRequests) async {
+    return await _appServicesClient.login(
+        loginRequests.email,
+        loginRequests.password
+    );
+  }
+  
+}
