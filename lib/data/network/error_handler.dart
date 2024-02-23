b62a9676e5adc5e76faa91e698ea5e8e944c5947
esraa-mohamad <78769599+esraa-mohamad@users.noreply.github.com>
2024-02-23 22:53:00 +0200

    response message for enum datasource

--- lib/data/network/error_handler.dart
+++ lib/data/network/error_handler.dart
@@ -32,3 +32,22 @@ class ResponseCode {
   static const int NO_INTERNET_CONNECTION = -6 ;
   static const int UNKNOWN = -7 ;
 }
+
+class ResponseMessage {
+  static const String SUCCESS = "Success" ; // success with data
+  static const String NO_CONTENT = "Success" ; // success without data (no content)
+  static const String BAD_REQUEST = "Bad request  , try again later" ; // failure, API rejected request
+  static const String UNAUTHORIZED = "User is unauthorized , try again later" ;  // failure , user is not authorized
+  static const String FORBIDDEN = "Forbidden request  , try again later" ; // failure, API rejected request
+  static const String INTERNET_SERVER_ERROR = "Something went wrong , try again later" ; // failure , crash in server side
+
+
+  // local status code
+  static const String CONNECT_TIMEOUT = "Time out error , try again later" ;
+  static const String CANCEL = "Request was canceled , try again later" ;
+  static const String RECEIVE_TIMEOUT = "Time out error , try again later" ;
+  static const String SEND_TIMEOUT = "Time out error , try again later" ;
+  static const String CASH_ERROR = "Cash error , try again later" ;
+  static const String NO_INTERNET_CONNECTION = "Please check your internet connection" ;
+  static const String UNKNOWN = "Something went wrong , try again later" ;
+}
