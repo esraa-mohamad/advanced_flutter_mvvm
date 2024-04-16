1ec05dddc143899325a6884a95ff480f8425b72a
esraa-mohamad <78769599+esraa-mohamad@users.noreply.github.com>
2024-04-16 17:34:47 +0200

    home UI , adding stores

--- lib/presentation/screens/main/pages/home/view/home_page.dart
+++ lib/presentation/screens/main/pages/home/view/home_page.dart
@@ -1,14 +1,12 @@
-import 'dart:js_interop';
 
 import 'package:advanced_flutter/app/di.dart';
 import 'package:advanced_flutter/presentation/common/state_render/state_renderer_imp.dart';
 import 'package:advanced_flutter/presentation/resources/color_manager.dart';
+import 'package:advanced_flutter/presentation/resources/routes_manager.dart';
 import 'package:advanced_flutter/presentation/resources/values_manager.dart';
 import 'package:advanced_flutter/presentation/screens/main/pages/home/home_viewModel/home_viewModel.dart';
 import 'package:carousel_slider/carousel_slider.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 import '../../../../../../domain/model/model.dart';
 import '../../../../../resources/strings_manager.dart';
@@ -108,6 +106,7 @@ class _HomePageState extends State<HomePage> {
       return Container();
     }
   }
+
   // *** sections ***
   Widget _getSections(String title){
     return  Padding(
@@ -175,7 +174,7 @@ class _HomePageState extends State<HomePage> {
                             child: Text(
                               service.title,
                               textAlign: TextAlign.center,
-                              style: Theme.of(context).textTheme.caption,
+                              style: Theme.of(context).textTheme.displaySmall,
                             ),
                           ),
                         ),
@@ -189,9 +188,58 @@ class _HomePageState extends State<HomePage> {
       return Container();
     }
   }
-  // *** stores ***
+
+  // *** stream stores ***
   Widget _getStores(){
-    return Center();
+    return StreamBuilder<List<Stores>>(
+        stream: _homeViewModel.outputStores,
+        builder: (context , snapshot){
+          return _getStoresWidget(snapshot.data);
+        }
+    );
+  }
+
+  // *** stores data lis ***
+  Widget _getStoresWidget(List<Stores>? stores){
+    if(stores != null){
+      return Padding(
+          padding: const EdgeInsets.only(
+            left: AppPadding.p12,
+            right: AppPadding.p12,
+            top: AppPadding.p12
+          ),
+          child: Flex(
+            direction: Axis.vertical,
+            children: [
+              GridView.count(
+                crossAxisCount: AppSize.s2,
+                crossAxisSpacing: AppSize.s8,
+                mainAxisSpacing: AppSize.s8,
+                physics: const ScrollPhysics(),
+                shrinkWrap: true,
+                children:
+                  List.generate(
+                      stores.length,
+                      (index) {
+                        return InkWell(
+                          onTap: (){
+                            // navigator to stores details
+                            Navigator.of(context).pushNamed(Routes.storeDetailsRoute);
+                          },
+                          child: Card(
+                            elevation: AppSize.s4,
+                            child: Image.network(stores[index].image , fit: BoxFit.cover, ),
+                          ),
+                        );
+                      }
+                  ),
+              ),
+            ],
+          ),
+      );
+    }else{
+      return Container();
+    }
   }
 
   @override
