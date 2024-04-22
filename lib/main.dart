afc090e663a742b78b22933adc3480e7d69ee4e9
esraa-mohamad <78769599+esraa-mohamad@users.noreply.github.com>
2024-04-22 23:41:11 +0200

    update main function with localization

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:advanced_flutter/presentation/resources/language_manager.dart';
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_phoenix/flutter_phoenix.dart';
 
 import 'app/app.dart';
 import 'app/di.dart';
@@ -6,8 +9,15 @@ import 'app/di.dart';
 void main() async{
 
   WidgetsFlutterBinding.ensureInitialized();
+  await EasyLocalization.ensureInitialized();
   await initAppModule();
-  runApp( MyApp());
+  runApp(
+    EasyLocalization(
+        supportedLocales: const [ARABIC_LOCALE , ENGLISH_LOCALE],
+        path: ASSET_PATH_LOCALIZATION,
+        child: Phoenix(child: MyApp(),)
+    ),
+  );
 }
 
 
