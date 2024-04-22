eb7dbc55de75ee73db3745c89e61f3391738b2a7
esraa-mohamad <78769599+esraa-mohamad@users.noreply.github.com>
2024-04-22 23:51:22 +0200

    update application class

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,8 +1,10 @@
+import 'package:advanced_flutter/app/app_prefs.dart';
+import 'package:advanced_flutter/app/di.dart';
 import 'package:advanced_flutter/presentation/resources/routes_manager.dart';
 import 'package:flutter/material.dart';
 
 import '../presentation/resources/theme_manager.dart';
-
+import 'package:easy_localization/easy_localization.dart';
 class MyApp extends  StatefulWidget {
 
   // default constructor , It is not instance  يعني عايزة استدعي الكونستراكتور ده مرة واحده
@@ -20,9 +22,21 @@ class MyApp extends  StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+
+  final AppPreferences _appPreferences = instance<AppPreferences>();
+
+  @override
+  void didChangeDependencies() {
+    _appPreferences.getLocal().then((local) => {context.setLocale(local)});
+    super.didChangeDependencies();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      localizationsDelegates: context.localizationDelegates,
+      supportedLocales: context.supportedLocales,
+      locale: context.locale,
       theme: getApplicationTheme(),
       debugShowCheckedModeBanner: false,
       onGenerateRoute: RouteGenerator.getRoute,
